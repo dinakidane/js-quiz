2c0cc0ed7abdcd4fc198d16be96fe8402e36454d
dinakidane <>
2023-05-03 18:04:32 +0000

    add start quiz and showquiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,5 +35,25 @@ const questions = [
             { text: "Africa", correct: false },
         ]
     }
-    
+
 ];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+}
